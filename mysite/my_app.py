102e276c72a7149a4c5d0a2c2c7d8c6bfc728ec7
Elena Zakharova <1583253@gmail.com>
2017-04-30 02:59:26 +0300

    дз vk api

--- mysite/my_app.py
+++ mysite/my_app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template, request
 from pymystem3 import Mystem
+import json
+import requests
 
 m = Mystem()
 app = Flask(__name__)
@@ -16,7 +18,7 @@ def verbs_info(text):
     for i in ana:
         if i['text'].strip() and 'analysis' in i and i['analysis']:
             word_cnt += 1
-            if 'V,' in i['analysis'][0]['gr']:
+            if 'V' in i['analysis'][0]['gr']:
                 verb_cnt += 1
                 if i['analysis'][0]['lex'] not in lem_freq_dict:
                     lem_freq_dict[i['analysis'][0]['lex']] = 1
@@ -41,13 +43,63 @@ def verbs_info(text):
     return res1, res2
 
 
+def vk_api(method, **kwargs):
+    api_request = 'https://api.vk.com/method/'+method + '?'
+    api_request += '&'.join(['{}={}'.format(key, kwargs[key]) for key in kwargs])
+    return json.loads(requests.get(api_request).text)
+
+
+def get_all_members(group_id, count):
+    member_ids = []
+    off = 0
+    while off < count:
+        current_ids = vk_api(method='groups.getMembers', group_id=group_id, offset=off, count=1000)
+        # print(current_ids)
+        member_ids.extend(current_ids['response']['users'])
+        off += 1000
+    return set(member_ids)
+
+
+def get_members_info(id1, id2):
+    id1_members_cnt = 0
+    id2_members_cnt = 0
+    closed = False
+    id1_ = vk_api(method='groups.getById', group_id=id1, fields='members_count')
+    id2_ = vk_api(method='groups.getById', group_id=id2, fields='members_count')
+    if id1_['response'][0]['is_closed'] == 1 or id2_['response'][0]['is_closed'] == 1:
+        closed = True
+        intersection = 0
+    else:
+        id1_members_cnt = id1_['response'][0]['members_count']
+        id2_members_cnt = id2_['response'][0]['members_count']
+        member_ids1 = get_all_members(id1, id1_members_cnt)
+        member_ids2 = get_all_members(id2, id2_members_cnt)
+        intersection = len(member_ids1.intersection(member_ids2))
+    return [id1_members_cnt, id2_members_cnt, intersection, closed]
+
+
 @app.route('/', methods=['get', 'post'])
 def index():
+    return render_template('index.html')
+
+
+@app.route('/verbs', methods=['get', 'post'])
+def verbs():
     if request.form:
         text = request.form['text']
         res1, res2 = verbs_info(text)
-        return render_template('index.html', input=text, toprint1=res1, toprint2=res2)
-    return render_template('index.html')
+        return render_template('verbs.html', input=text, toprint1=res1, toprint2=res2)
+    return render_template('verbs.html')
+
+
+@app.route('/vk', methods=['get', 'post'])
+def vk():
+    if request.form:
+        group_id1 = request.form['group_id1']
+        group_id2 = request.form['group_id2']
+        result = get_members_info(group_id1, group_id2)
+        return render_template('vk.html', result=result, group_id1 = group_id1, group_id2 = group_id2)
+    return render_template('vk.html')
 
 
 if __name__ == '__main__':
